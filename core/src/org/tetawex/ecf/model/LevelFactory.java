adfba4e61f82312e8a60ff3037a3be17acdd6701
Vladimir K <mail@kemerover.com>
2017-06-11 13:34:40 +0400

    Implemented LevelFactory with JSON upload

--- core/src/org/tetawex/ecf/model/LevelFactory.java
+++ core/src/org/tetawex/ecf/model/LevelFactory.java
@@ -3,6 +3,11 @@ package org.tetawex.ecf.model;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
+import com.badlogic.gdx.utils.OrderedSet;
+import com.badlogic.gdx.utils.XmlReader;
+
 import org.tetawex.ecf.util.IntVector2;
 
 /**
@@ -11,75 +16,45 @@ import org.tetawex.ecf.util.IntVector2;
 public class LevelFactory {
     //TODO Replace with an adequate loading system
     public static LevelData generateLevel(int number){
-        LevelData data=new LevelData();
-        //Указать параметры
-        data.setMana(1);
-        data.setMaxScore(4000);
-        //Это объект для считывания
-        Json json=new Json();
-        //Объект для считывания, класс можно сделать например на jsonschema2pojo.org
-        YourJsonObject obj=json.fromJson(YourJsonObject.class,
-                readFile("levels/level"+number+".json"));
+        JsonReader jsonReader = new JsonReader();
+        JsonValue jsonData = jsonReader.parse(readFile("levels/level"+number+".json"));
+        int height = jsonData.get("height").asInt();
+        int width = jsonData.get("width").asInt();
+        int mana = jsonData.get("properties").get("mana").asInt();
+        Cell[][] cellArray = new Cell[width][height];
 
-        //...Делаешь какие-то нетривиальные преобразования и возвращаешь LevelData
+        JsonValue.JsonIterator layersIter = jsonData.get("layers").iterator();
+        while (layersIter.hasNext()) {
+            JsonValue layer = layersIter.next();
+            JsonValue.JsonIterator dataIter = layer.get("data").iterator();
+            int i = 0;
+            while(dataIter.hasNext()){
+                int     x = i%width,
+                        y = height-1-i/width;
+                switch(dataIter.next().asInt()){
+                    case 1:  cellArray[x][y] = CellFactory.generateEmptyCell(new IntVector2(x,y));
+                        break;
+                    case 2:  cellArray[x][y].getElements().add(Element.FIRE);
+                        break;
+                    case 3:  cellArray[x][y].getElements().add(Element.WATER);
+                        break;
+                    case 4:  cellArray[x][y].getElements().add(Element.SHADOW);
+                        break;
+                    case 5:  cellArray[x][y].getElements().add(Element.LIGHT);
+                        break;
+                    case 6:  cellArray[x][y].getElements().add(Element.AIR);
+                        break;
+                    case 7:  cellArray[x][y].getElements().add(Element.EARTH);
+                        break;
+                    default: break;
+                }
+                ++i;
+            }
+        }
+
+        LevelData data = new LevelData(cellArray, mana, 4000);
+
         return data;
-
-
-
-        //Cell[][] cellArray= CellArrayFactory.generateEmptyCellArray(3,3);
-        /*if(number==0) {
-            cellArray = CellArrayFactory.generateEmptyCellArray(3,3);
-            cellArray[0][0] = null;
-            cellArray[0][1] = null;
-            cellArray[2][0] = null;
-            cellArray[2][1] = null;
-
-            cellArray[0][2].setElements(Element.WATER, Element.EARTH);
-            cellArray[1][1].setElements(Element.FIRE, Element.EARTH, Element.SHADOW);
-            cellArray[1][0].setElements(Element.FIRE, Element.AIR, Element.LIGHT);
-            cellArray[2][2].setElements(Element.WATER);
-            cellArray[1][2].setElements(Element.AIR);
-        }
-        else if(number==1) {
-            cellArray = CellArrayFactory.generateEmptyCellArray(3,3);
-            cellArray[0][2] = null;
-            cellArray[2][2] = null;
-            cellArray[2][0] = null;
-
-            cellArray[0][0].setElements(Element.WATER, Element.AIR, Element.LIGHT);
-            cellArray[1][0].setElements(Element.SHADOW, Element.EARTH);
-            cellArray[2][1].setElements(Element.AIR, Element.SHADOW, Element.WATER);
-            cellArray[0][1].setElements(Element.LIGHT, Element.AIR, Element.FIRE);
-            cellArray[1][1].setElements(Element.LIGHT, Element.EARTH, Element.FIRE);
-            cellArray[1][2].setElements(Element.SHADOW, Element.WATER);
-        }
-        else if(number==2) {
-            cellArray = CellArrayFactory.generateEmptyCellArray(3,3);
-            cellArray[0][2] = null;
-            cellArray[2][0] = null;
-
-            cellArray[0][0].setElements(Element.SHADOW);
-            cellArray[0][1].setElements(Element.FIRE);
-            cellArray[1][0].setElements(Element.EARTH, Element.SHADOW);
-            cellArray[1][1].setElements(Element.SHADOW, Element.AIR);
-            cellArray[1][2].setElements(Element.LIGHT, Element.WATER, Element.AIR);
-            cellArray[2][1].setElements(Element.FIRE, Element.SHADOW, Element.EARTH);
-            cellArray[2][2].setElements(Element.WATER, Element.SHADOW);
-        }
-        else if(number==3) {
-            cellArray = CellArrayFactory.generateEmptyCellArray(3,3);
-            cellArray[0][0] = null;
-            cellArray[2][0] = null;
-
-            cellArray[0][1].setElements(Element.SHADOW, Element.AIR);
-            cellArray[0][2].setElements(Element.FIRE, Element.EARTH);
-            cellArray[1][0].setElements(Element.LIGHT, Element.WATER);
-            cellArray[1][1].setElements(Element.SHADOW, Element.EARTH, Element.FIRE);
-            cellArray[1][2].setElements(Element.LIGHT, Element.WATER);
-            cellArray[2][1].setElements(Element.SHADOW, Element.AIR);
-            cellArray[2][2].setElements(Element.EARTH, Element.FIRE);
-        }
-        data.setCellArray(cellArray);*/
     }
     private static String readFile(String fileName){
         FileHandle handle = Gdx.files.internal(fileName);
