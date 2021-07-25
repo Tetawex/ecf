fa76f453c50dd91ae4d126316c3940623cd5c980
Tetawex <tetawex@gmail.com>
2021-07-25 14:07:20 +0300

    Improve back button support

--- core/src/org/tetawex/ecf/core/ActionResolverAdapter.kt
+++ core/src/org/tetawex/ecf/core/ActionResolverAdapter.kt
@@ -1,10 +1,12 @@
 package org.tetawex.ecf.core
 
+import com.badlogic.gdx.Gdx
+
 /**
  * Created by Tetawex on 12.06.2017.
  */
 class ActionResolverAdapter : ActionResolver {
-    override fun setBackPressedListener(listener: (didHandle: Boolean) -> Unit) = Unit
+    override fun setBackPressedListener(listener: () -> Boolean) =Unit
     override fun externalStorageAccessible() = false
     override fun openUrl(url: String) = Unit
 }
