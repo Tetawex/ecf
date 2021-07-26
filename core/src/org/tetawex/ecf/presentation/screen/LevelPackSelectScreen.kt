18655ba85a7640fe32ba317c52e0e1cb72e18d0b
Tetawex <tetawex@gmail.com>
2021-07-26 22:51:29 +0300

    Refactor BaseScreen.kt

--- core/src/org/tetawex/ecf/presentation/screen/LevelPackSelectScreen.kt
+++ core/src/org/tetawex/ecf/presentation/screen/LevelPackSelectScreen.kt
@@ -1,16 +1,12 @@
 package org.tetawex.ecf.presentation.screen
 
-import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.scenes.scene2d.Actor
-import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.scenes.scene2d.ui.Stack
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener
-import com.badlogic.gdx.utils.viewport.ExtendViewport
 import org.tetawex.ecf.core.ECFGame
 import org.tetawex.ecf.core.GameStateManager
 import org.tetawex.ecf.model.ECFPreferences
@@ -20,30 +16,31 @@ import org.tetawex.ecf.util.PreferencesProvider
 /**
  * Created by Tetawex on 03.06.2017.
  */
-class LevelPackSelectScreen(game: ECFGame, bundle: Bundle?) : BaseECFScreen(game) {
+class LevelPackSelectScreen(game: ECFGame, bundle: Bundle?) : BaseScreen(game) {
 
     private val preferences: ECFPreferences
 
-    private val stage: Stage
-
     init {
         preferences = PreferencesProvider.getPreferences()
-        if (!preferences.completedTutorial)
+        if (!preferences.completedTutorial) {
             game.gameStateManager.setState(GameStateManager.GameState.TUTORIAL, null)
-        val camera = OrthographicCamera(1440f, 2560f)
-        camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0f)
-        stage = Stage(ExtendViewport(1440f, 2560f, camera))
-        Gdx.input.inputProcessor = stage
+        }
+
         initUi()
     }
 
     private fun initUi() {
         val stack = Stack()
         stack.setFillParent(true)
-        stack.add(Image(
+        stack.add(
+            Image(
                 game.assetManager
-                        .get("backgrounds/background.png",
-                                Texture::class.java)))
+                    .get(
+                        "backgrounds/background.png",
+                        Texture::class.java
+                    )
+            )
+        )
         val mainTable = Table()
         mainTable.setFillParent(true)
         stack.add(mainTable)
@@ -57,25 +54,38 @@ class LevelPackSelectScreen(game: ECFGame, bundle: Bundle?) : BaseECFScreen(game
                 game.gameStateManager.setState(GameStateManager.GameState.LEVEL_SELECT, bundle)
             }
         })
-        val menuButtonMot = TextButton(game.getLocalisedString("lp_mot"), StyleFactory.generateMotMenuButtonStyle(game))
+        val menuButtonMot = TextButton(
+            game.getLocalisedString("lp_mot"),
+            StyleFactory.generateMotMenuButtonStyle(game)
+        )
         menuButtonMot.addListener(object : ChangeListener() {
             override fun changed(event: ChangeListener.ChangeEvent, actor: Actor) {
                 if (preferences.completedMotTutorial) {
                     if (!preferences.completedMotTutorial) {
-                        game.gameStateManager.setState(GameStateManager.GameState.MOT_TUTORIAL, null)
+                        game.gameStateManager.setState(
+                            GameStateManager.GameState.MOT_TUTORIAL,
+                            null
+                        )
                     } else {
                         val bundle = Bundle()
                         bundle.putItem("levelCode", "mot")
-                        game.gameStateManager.setState(GameStateManager.GameState.LEVEL_SELECT, bundle)
+                        game.gameStateManager.setState(
+                            GameStateManager.GameState.LEVEL_SELECT,
+                            bundle
+                        )
                     }
                 } else
                     game.gameStateManager.setState(GameStateManager.GameState.MOT_TUTORIAL, null)
             }
         })
 
-        val menuButtonSc = TextButton(game.getLocalisedString("lp_sc"), StyleFactory.generateScMenuButtonStyle(game))
+        val menuButtonSc = TextButton(
+            game.getLocalisedString("lp_sc"),
+            StyleFactory.generateScMenuButtonStyle(game)
+        )
 
-        val menuButtonBackPlayModeSelectScreen = TextButton(game.getLocalisedString("back"), tbStyle)
+        val menuButtonBackPlayModeSelectScreen =
+            TextButton(game.getLocalisedString("back"), tbStyle)
         menuButtonBackPlayModeSelectScreen.addListener(object : ChangeListener() {
             override fun changed(event: ChangeListener.ChangeEvent, actor: Actor) {
                 onBackPressed()
@@ -84,17 +94,8 @@ class LevelPackSelectScreen(game: ECFGame, bundle: Bundle?) : BaseECFScreen(game
         mainTable.add(menuButtonClassic).size(BUTTON_WIDTH, BUTTON_HEIGHT).pad(BUTTON_PAD).row()
         mainTable.add(menuButtonMot).size(BUTTON_WIDTH, BUTTON_HEIGHT).pad(BUTTON_PAD).row()
         //mainTable.add(menuButtonSc).size(BUTTON_WIDTH, BUTTON_HEIGHT).pad(BUTTON_PAD).row()
-        mainTable.add(menuButtonBackPlayModeSelectScreen).size(BUTTON_WIDTH, BUTTON_HEIGHT).pad(BUTTON_PAD).row()
-    }
-
-    override fun render(delta: Float) {
-        stage.act(delta)
-        stage.draw()
-    }
-
-    override fun resize(width: Int, height: Int) {
-        stage.viewport.update(width, height, true)
-        stage.viewport.camera.update()
+        mainTable.add(menuButtonBackPlayModeSelectScreen).size(BUTTON_WIDTH, BUTTON_HEIGHT)
+            .pad(BUTTON_PAD).row()
     }
 
     override fun onBackPressed(): Boolean {
